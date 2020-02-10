45e9fb1f54cd660e7f4d5b25b8cc1c808cd7255c
Mie Chayakorn <miechayakorn@gmail.com>
2020-02-10 15:03:07 +0700

    [Create] user controller

---
+++ bbq-backend/app/Controllers/Http/UserController.js
@@ -0,0 +1,31 @@
+"use strict";
+
+class UserController {
+  async store({ request, response }) {
+    try {
+      // getting data passed within the request
+      const data = request.only(["username", "email", "password"]);
+
+      console.log(data);
+
+      /*// looking for user in database
+      const userExists = await User.findBy("email", data.email);
+
+      // if user exists don't save
+      if (userExists) {
+        return response
+          .status(400)
+          .send({ message: { error: "User already registered" } });
+      }
+
+      // if user doesn't exist, proceeds with saving him in DB
+      const user = await User.create(data);
+
+      return user;*/
+    } catch (err) {
+      return response.status(err.status).send(err);
+    }
+  }
+}
+
+module.exports = UserController;
