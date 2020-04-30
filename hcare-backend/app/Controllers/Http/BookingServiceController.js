21a738de1ff89e075d6329dcc8d78b7beccad0f9
Kridtakom <kridtakom.boom@mail.kmutt.ac.th>
2020-04-30 22:53:39 +0700

    [Add] Check Token

--- hcare-backend/app/Controllers/Http/BookingServiceController.js
+++ hcare-backend/app/Controllers/Http/BookingServiceController.js
@@ -23,29 +23,30 @@ class BookingServiceController {
       return response.status(error.status).send(error);
     }
   }
-  //   async update({ request, response }) {
-  //     let data = await request.only([
-  //       "type_id",
-  //       "time_slot",
-  //       "start_time",
-  //       "end_time",
-  //       "creator_id"
-  //     ]);
-  //     let type = Database.from("types").where({ type_id: data.type_id });
-  //     console.log(type);
-  //     if (type === null) {
-  //       return "No have this type in database";
-  //     }
-  //     let updateType = await Database.table("types")
-  //       .where("type_id", data.type_id)
-  //       .update({
-  //         time_slot: data.time_slot,
-  //         start_time: data.start_time,
-  //         end_time: data.end_time,
-  //         creator_id: data.creator_id
-  //       });
-  //     return await Database.from("types").where({ type_id: data.type_id });
-  //   }
+    
+  async update({ request, response }) {
+      let data = await request.only([
+        "type_id",
+        "time_slot",
+        "start_time",
+        "end_time",
+        "creator_id"
+      ]);
+      let type = Database.from("types").where({ type_id: data.type_id });
+      console.log(type);
+      if (type === null) {
+        return "No have this type in database";
+      }
+      let updateType = await Database.table("types")
+        .where("type_id", data.type_id)
+        .update({
+          time_slot: data.time_slot,
+          start_time: data.start_time,
+          end_time: data.end_time,
+          creator_id: data.creator_id
+        });
+      return await Database.from("types").where({ type_id: data.type_id });
+    }
 }
 
 module.exports = BookingServiceController;
