6923eb49d7220edca339db2c9ff310ce36e60ccc
Mie Chayakorn <miechayakorn@gmail.com>
2020-08-25 11:29:55 +0700

    [Edit] check commit branch in env

--- hcare-backend/init.sh
+++ hcare-backend/init.sh
@@ -5,5 +5,13 @@ echo DB_PASSWORD=$MYSQL_PASSWORD >> .env.docker
 echo DB_DATABASE=$MYSQL_DATABASE >> .env.docker
 echo MAIL_USERNAME=$MAIL_USERNAME >> .env.docker
 echo MAIL_PASSWORD=$MAIL_PASSWORD >> .env.docker
-echo VUE_APP_FONTEND_URL=$VUE_APP_FONTEND_URL >> .env.docker
-echo VUE_APP_BACKEND_URL=$VUE_APP_BACKEND_URL >> .env.docker
+
+if [ "$CI_COMMIT_BRANCH" == "develop" ]; then
+    echo "------- + $CI_COMMIT_BRANCH + -------- develop"
+    echo VUE_APP_FONTEND_URL=$VUE_APP_FONTEND_URL_DEV >> .env.docker
+    echo VUE_APP_BACKEND_URL=$VUE_APP_BACKEND_URL_DEV >> .env.docker
+else
+    echo "------- + $CI_COMMIT_BRANCH + -------- else"
+    echo VUE_APP_FONTEND_URL=$VUE_APP_FONTEND_URL >> .env.docker
+    echo VUE_APP_BACKEND_URL=$VUE_APP_BACKEND_URL >> .env.docker
+fi
